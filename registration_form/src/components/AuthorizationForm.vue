fc813ab4029c6a8efc545053692f23fc5b1aeba4
Максим <akaikinmax@mail.ru>
2023-12-17 03:42:31 +0700

    Стили вынесены в отдельные файлы

--- registration_form/src/components/AuthorizationForm.vue
+++ registration_form/src/components/AuthorizationForm.vue
@@ -4,15 +4,17 @@
     <div class="content">
         <form action="">
             <h2 class="content__title">Authorization</h2>
-            <input type="text" name="login" class="content__input" placeholder="Login" required="required" />
-            <input type="password" name="password" class="content__input" placeholder="Password" />
-            <button class="content__btn">Login</button>
+            <input type="text" name="login" class="input" placeholder="Login" required="required" />
+            <input type="password" name="password" class="input" placeholder="Password" />
+            <button class="button">Login</button>
         </form>
     </div>
 </template>
 
 <style scoped lang="sass">
     @import '../assets/fonts.css'
+    @import '../assets/styles/blocks/input.sass'
+    @import '../assets/styles/blocks/button.sass'
     .content
         color: #FFF
         font-family: "Roboto", sans-serif
@@ -28,36 +30,4 @@
         form
             display: flex
             flex-direction: column
-        &__input
-            margin-bottom: 11px
-            width: 100%
-            height: 36px
-            box-sizing: border-box
-            padding: 9px 19px
-            background: #444
-            border-radius: 18px
-            border: 1px solid #23eac6
-            color: rgba(35, 234, 198, 0.40)
-            font-family: "Roboto", sans-serif
-            font-size: 15px
-            font-weight: 500
-            :active
-                background-color: #fff
-            &_file
-                margin-top: 16px
-        &__btn
-            width: 326px
-            height: 56px
-            box-sizing: border-box
-            text-align: center
-            margin: 0 auto
-            margin-top: 40px
-            border: none
-            border-radius: 28px
-            background: #E9A21A
-            color: #FFF
-            font-family: "Roboto", sans-serif
-            font-size: 20px
-            font-weight: 400
-            text-transform: uppercase
 </style>
--- registration_form/src/components/RegistrationForm.vue
+++ registration_form/src/components/RegistrationForm.vue
@@ -1,22 +1,44 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+let person = ref({
+    firstName: String,
+    lastName: String,
+    login: String,
+    emil: String,
+    password: String,
+    photo: null
+})
+
+const formElement = document.getElementById('registr');
+
+function createAccount(event){
+    event.preventDefault();
+    const formData = new FormData(formElement);
+
+}
+
+</script>
 
 <template>
     <div class="content">
-        <form action="">
+        <form id="registr">
             <h2 class="content__title">Create Account</h2>
-            <input type="text" name="firstName" class="content__input" placeholder="First Name" required="required" />
-            <input type="text" name="lastName" class="content__input" placeholder="Last Name" required="required" />
-            <input type="text" name="login" class="content__input" placeholder="Login" required="required" />
-            <input type="email" name="email" class="content__input" placeholder="Email" required="required" />
-            <input type="password" name="password" class="content__input" placeholder="Password" />
-            <input type="file" name="file" class="content__input_file" />
-            <button class="content__btn">create</button>
+            <input type="text" name="firstName" class="input" placeholder="First Name" required="required" />
+            <input type="text" name="lastName" class="input" placeholder="Last Name" required="required" />
+            <input type="text" name="login" class="input" placeholder="Login" required="required" />
+            <input type="email" name="email" class="input" placeholder="Email" required="required" />
+            <input type="password" name="password" class="input" placeholder="Password" />
+            <input type="file" name="photo" class="input input_file" />
+            <button class="button" @click="createAccount">create</button>
         </form>
     </div>
 </template>
 
 <style scoped lang="sass">
     @import '../assets/fonts.css'
+    @import '../assets/styles/blocks/input.sass'
+    @import '../assets/styles/blocks/button.sass'
     .content
         color: #FFF
         font-family: "Roboto", sans-serif
@@ -32,36 +54,4 @@
         form
             display: flex
             flex-direction: column
-        &__input
-            margin-bottom: 11px
-            width: 100%
-            height: 36px
-            box-sizing: border-box
-            padding: 9px 19px
-            background: #444
-            border-radius: 18px
-            border: 1px solid #23eac6
-            color: rgba(35, 234, 198, 0.40)
-            font-family: "Roboto", sans-serif
-            font-size: 15px
-            font-weight: 500
-            :active
-                background-color: #fff
-            &_file
-                margin-top: 16px
-        &__btn
-            width: 326px
-            height: 56px
-            box-sizing: border-box
-            text-align: center
-            margin: 0 auto
-            margin-top: 40px
-            border: none
-            border-radius: 28px
-            background: #E9A21A
-            color: #FFF
-            font-family: "Roboto", sans-serif
-            font-size: 20px
-            font-weight: 400
-            text-transform: uppercase
 </style>
